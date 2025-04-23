bd5532ff70c7e9b73c28354021d88f87741a20ab
mudit-51 <mudit.7.gupta+github@gmail.com>
2025-04-23 11:10:18 +0530

    feat: vendor management

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask
+from systemdataclasses import vendor
+
+vendor_list = vendor.VendorList()
+v1 = vendor.Vendor("V001", "Acme Corp", "123-456-7890")
+v2 = vendor.Vendor("V002", "Globex Inc", "987-654-3210")
+
+vendor_list.add_vendor(v1)
+vendor_list.add_vendor(v2)
 
 app = Flask(__name__)
+
+@app.route('/vendors', methods=['GET'])
+def get_vendors():
+    return vendor_list.toJson()
 
 @app.route('/')
 def hello_world():
--- systemdataclasses/vendor.py
+++ systemdataclasses/vendor.py
@@ -6,3 +6,26 @@ class Vendor:
 
     def __str__(self):
         return f"Vendor ID: {self.vendor_id}, Name: {self.name}, Contact Info: {self.contact_info}"
+    
+    def toJson(self):
+        return {
+            "vendor_id": self.vendor_id,
+            "name": self.name,
+            "contact_info": self.contact_info
+        }
+
+class VendorList:
+    def __init__(self):
+        self.vendors = []
+
+    def add_vendor(self, vendor: Vendor):
+        self.vendors.append(vendor)
+
+    def remove_vendor(self, vendor_id: str):
+        self.vendors = [vendor for vendor in self.vendors if vendor.vendor_id != vendor_id]
+        
+    def get_vendors(self):
+        return self.vendors
+
+    def toJson(self):
+        return [vendor.toJson() for vendor in self.vendors]
